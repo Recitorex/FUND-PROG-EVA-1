f7614694f29c1356b814da9a26505d8733f03dfa
Recitorex <143562580+Recitorex@users.noreply.github.com>
2023-09-19 17:48:33 -0600

    Add files via upload

---
+++ EVA1_10_INICIALIZACION/src/eva1_10_inicializacion/EVA1_10_INICIALIZACION.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_10_inicializacion;
+
+/**
+ *
+ * @author Alberto
+ */
+public class EVA1_10_INICIALIZACION {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int valor = 0; //inicializando la variable 
+        System.out.println(valor);
+
+        int precio;
+        precio = 100;
+        double salario = 0, precioMayoreo = 100, iva = 10.5, subtotal = 15, total = 100.0;// 5 variables todas de tipo double 
+
+        //constantes
+        //final --> te permite asignar un valor solo una vez
+        final int conteo;
+        conteo = 100;
+        System.out.println(conteo);
+
+        /*final double MI_VALOR_PI = 3.1416;//USAR MAYUSCULAS
+        final String ESCUELA = "instituto tecnologixo de chihuahua II";
+        final int CALIFA_PASAR = 70;*/
+        /*
+        variables lineas de comentarios cuando queremos escribir mucho
+        este funciona */
+        
+        
+        
+        int x;//comentario de una sola linea, todo en la linea es un comentario 
+
+    }
+
+}
