66f36f29a3be0eb43df4660dab1dd7b87dfd8f4a
Recitorex <143562580+Recitorex@users.noreply.github.com>
2023-09-19 17:48:58 -0600

    Add files via upload

---
+++ EVA_11_TIPOS_DE_DATOS/src/eva_11_tipos_de_datos/EVA_11_TIPOS_DE_DATOS.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_11_tipos_de_datos;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Alberto
+ */
+public class EVA_11_TIPOS_DE_DATOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        byte califa;
+        califa = 127;
+        //califa =200;
+        califa=-128;
+        //califa = -200;
+        //califa=127+1;
+        int valor;
+        valor = Integer.MAX_VALUE;
+        System.out.println(valor+1);
+        valor=Integer.MIN_VALUE;
+        System.out.println(valor);
+        
+        //-----------
+        char caracter='@';
+        System.out.println(caracter);
+        System.out.println(caracter + 0);
+        
+                
+        
+        
+      
+        
+        // TODO code application logic here
+    }
+    
+}
